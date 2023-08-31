64953e52e8f86dbdba28644b3542ba3e391c7258
Taro Schenker <taroschenker@hotmail.co.uk>
2023-08-31 16:56:59 +0100

    app test passing mongo memory server added

---
+++ server/__tests__/app.test.ts
@@ -0,0 +1,33 @@
+import request from "supertest";
+import app from "../src/app"; // Assuming you have exported your Express app instance.
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+let mongoServer: MongoMemoryServer;
+
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+
+  await mongoose.connect(uri);
+});
+
+beforeEach(async () => {
+});
+
+afterEach(async () => {
+  await mongoose.connection.dropDatabase();
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+describe("App routes", () => {
+  test("GET / should return Hello World", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("API is running");
+  });
+});
