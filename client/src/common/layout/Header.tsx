cee8bdf7247a82ff1692ce8ba9d47bd3102a9a27
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-01 23:45:19 +0100

    add icons

--- client/src/common/layout/Header.tsx
+++ client/src/common/layout/Header.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faSearch, faHeart, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 interface HeaderProps {
   sidebarOpen: boolean;
@@ -11,35 +13,31 @@ const Header: React.FC<HeaderProps> = ({ sidebarOpen, setSidebarOpen }) => {
     <header className="bg-primary-color text-background-color py-4">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">Cashmere Store</h1>
-        <nav className="hidden md:flex">
-          <NavLink 
-            to="/" 
-            className="mr-4" 
-            activeClassName="text-secondary-color"
-            exact
-          >
+        <nav className="hidden md:flex space-x-4">
+          <NavLink to="/" activeClassName="text-secondary-color" exact>
             Home
           </NavLink>
-          <NavLink 
-            to="/products" 
-            className="mr-4" 
-            activeClassName="text-secondary-color"
-          >
+          <NavLink to="/products" activeClassName="text-secondary-color">
             Shop
           </NavLink>
-          <NavLink 
-            to="/about" 
-            className="mr-4" 
-            activeClassName="text-secondary-color"
-          >
+          <NavLink to="/about" activeClassName="text-secondary-color">
             About
           </NavLink>
-          <NavLink 
-            to="/contact" 
-            className="mr-4" 
-            activeClassName="text-secondary-color"
-          >
+          <NavLink to="/contact" activeClassName="text-secondary-color">
             Contact
+          </NavLink>
+          {/* Icons */}
+          <NavLink to="/search">
+            <FontAwesomeIcon icon={faSearch} className="text-lg" />
+          </NavLink>
+          <NavLink to="/favorites">
+            <FontAwesomeIcon icon={faHeart} className="text-lg" />
+          </NavLink>
+          <NavLink to="/cart">
+            <FontAwesomeIcon icon={faShoppingCart} className="text-lg" />
+          </NavLink>
+          <NavLink to="/user/auth">
+            <FontAwesomeIcon icon={faUser} className="text-lg" />
           </NavLink>
         </nav>
         <div className="flex md:hidden items-center">
--- client/src/features/Products/components/ProductCard.tsx
+++ client/src/features/Products/components/ProductCard.tsx
@@ -7,16 +7,42 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-<div className="border rounded-lg p-4 bg-background-color">
-  <img src={product.imageUrl} alt={product.name} className="w-full h-48 object-cover" />
-  <h2 className="text-text-color font-body font-bold mt-2">{product.name}</h2>
-  <p className="text-neutral-dark mt-1">{product.description}</p>
-  <div className="mt-2 flex justify-between items-center">
-    <span className="font-bold text-primary-color">${product.price}</span>
-    <button className="bg-primary-color text-background-color px-3 py-1 rounded">Add to Cart</button>
-  </div>
-</div>
+    <div className="border rounded-lg p-4 bg-background-color hover:shadow-lg transition-shadow duration-300">
+      <div className="relative">
+        <img src={product.imageUrl} alt={product.name} className="w-full h-48 object-cover rounded-t-lg" />
+        {product.stockCount === 0 && (
+          <span className="absolute top-2 left-2 bg-neutral-dark text-background-color px-2 py-1 rounded">Out of Stock</span>
+        )}
+      </div>
+      <h2 className="text-text-color font-body font-bold mt-2">{product.name}</h2>
+      <p className="text-neutral-dark mt-1">{product.description}</p>
+      
+      <div className="flex mt-2 items-center">
+        <div className="flex-1">
+          <span className="text-sm text-neutral-dark">Sizes: {product.size.join(', ')}</span>
+        </div>
+        <span className="font-bold text-primary-color">£{product.price}</span>
+      </div>
 
+      <div className="flex mt-2 items-center">
+        <div className="flex-1">
+          <span className="text-sm text-neutral-dark">Colors: </span>
+          {product.color.map((c, index) => (
+            <span 
+              key={index} 
+              className="inline-block w-4 h-4 mr-1 rounded-full" 
+              style={{ backgroundColor: c }}
+            ></span>
+          ))}
+        </div>
+        <button 
+          className={`bg-primary-color text-background-color px-3 py-1 rounded ${product.stockCount === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+          disabled={product.stockCount === 0}
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
   );
 };  
 
