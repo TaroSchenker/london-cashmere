a1559cbae2ee33715e017256eecc3d172ff9c58a
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-03 00:00:38 +0100

    fixed overflowing text in banners

--- client/src/common/layout/ProductListHeader.tsx
+++ client/src/common/layout/ProductListHeader.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 // Import the image
 import luxuryImage from '../assets/images/stock-photo-stack-of-white-and-gray-cozy-knitted-sweaters-on-rustic-wooden-background-469992296.jpg'; 
-
 const ProductListHeader: React.FC = () => {
   return (
-    <div className="relative h-48 bg-center bg-cover mb-3" style={{ backgroundImage: `url(${luxuryImage})` }}>
+    <div className="relative h-48 md:h-64 lg:h-80 bg-center bg-cover mb-3" style={{ backgroundImage: `url(${luxuryImage})` }}>
       
       {/* Overlay to make text stand out on top of the image */}
       <div className="absolute inset-0 bg-black opacity-40"></div>
 
-      <div className="absolute top-1/4 left-1/4 text-white space-y-2 z-10"> 
-        <h1 className="text-4xl font-semibold tracking-wide">Exclusive Arrival</h1>
-        <p className="text-lg">Discover the essence of luxury fashion. Limited pieces, timeless elegance.</p>
+      <div className="absolute top-1/4 left-1/4 p-4 md:p-0 text-white space-y-2 z-10"> 
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold tracking-wide">Exclusive Arrival</h1>
+        <p className="text-sm md:text-lg">Discover the essence of luxury fashion. Limited pieces, timeless elegance.</p>
       </div>
     </div>
   );
--- client/src/common/layout/SaleBanner.tsx
+++ client/src/common/layout/SaleBanner.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-
 const SaleBanner: React.FC = () => {
-  return (
-    <div className="my-0.5 bg-red-600 text-white p-2 rounded-lg shadow-md">
-      <div className="text-center">
-        <h2 className="text-2xl font-semibold tracking-wide">Limited Time Sale!</h2>
-        <p className="mt-0">Get 20% off on select items. Hurry, while stocks last!</p>
+    return (
+      <div className="my-0.5 bg-red-600 text-white p-2 rounded-lg shadow-md">
+        <div className="text-center p-2">
+          <h2 className="text-xl md:text-2xl font-semibold tracking-wide">Limited Time Sale!</h2>
+          <p className="mt-0 text-sm md:text-base">Get 20% off on select items. Hurry, while stocks last!</p>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default SaleBanner;
+    );
+  };
+  
+  export default SaleBanner;
