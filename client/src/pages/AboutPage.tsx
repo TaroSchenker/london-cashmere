4cd0bb444676f40dc59d29a6a231ccb8646d04c5
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-02 18:35:16 +0100

    about page updated

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -7,52 +7,57 @@ import cashmereImage4 from '../common/assets/images/stock-photo-stack-of-white-a
 const CashmereInfo: React.FC = () => {
     return (
         <div className="p-8 bg-background-color">
-            <div className="max-w-5xl mx-auto border border-gray-300 rounded-lg hover:shadow-xl transition-shadow duration-300 bg-white">
+            <div className="max-w-6xl mx-auto border border-gray-300 rounded-lg hover:shadow-xl transition-shadow duration-300 bg-white">
                 
-                <div className="relative group">
-                    <img 
-                        src={cashmereImage}
-                        alt="Cashmere Info"
-                        className="w-full h-96 object-cover rounded-t-lg"
-                    />
-                    <h2 className="absolute bottom-10 left-10 text-5xl text-white font-serif font-bold">Cashmere</h2>
-                </div>
+                <div className="lg:flex">
 
-                <div className="p-6">
-                    <h2 className="text-black font-serif font-bold text-xl mb-2">The History of Cashmere</h2>
-                    <p className="text-gray-600 mb-4">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat id nunc in luctus. 
-                        Cras eget erat ut velit ultricies sollicitudin. Maecenas sed justo eu metus semper consectetur.
-                    </p>
+                    {/* Left Column for Image */}
+                    <div className="lg:w-2/5">
+                        <img 
+                            src={cashmereImage2}
+                            alt="Cashmere Info"
+                            className="w-full h-full object-cover rounded-t-lg lg:rounded-l-lg lg:rounded-t-none"
+                        />
+                    </div>
 
-                    <img 
-                        src={cashmereImage2}
-                        alt="Cashmere History"
-                        className="mb-4 w-full h-60 object-cover rounded"
-                    />
+                    {/* Right Column for Text and smaller images */}
+                    <div className="p-6 lg:w-3/5">
 
-                    <h2 className="text-black font-serif font-bold text-xl mb-2 mt-4">Caring for Cashmere</h2>
-                    <p className="text-gray-600 mb-4">
-                        Cashmere is a luxurious material and requires specific care. Follow our guide to ensure longevity.
-                    </p>
+                        <h2 className="text-black font-serif font-bold text-3xl mb-2">Cashmere</h2>
+                        <p className="text-gray-600 mb-4">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat id nunc in luctus. 
+                            Cras eget erat ut velit ultricies sollicitudin. Maecenas sed justo eu metus semper consectetur.
+                        </p>
 
-                    <ul className="list-disc pl-6 mb-4">
-                        <li className="mb-2">Wash with cold water using a gentle cycle or by hand.</li>
-                        <li className="mb-2">Avoid using bleach or fabric softeners on cashmere.</li>
-                        <li className="mb-2">Dry flat and keep away from direct sunlight or heat sources.</li>
-                        <li>Store in a cool, dry place.</li>
-                    </ul>
+                        <img 
+                            src={cashmereImage4}
+                            alt="Cashmere History"
+                            className="mb-4 w-full h-72 object-cover rounded"
+                        />
 
-                    <img 
-                        src={cashmereImage3}
-                        alt="Cashmere Care"
-                        className="mb-4 w-full h-60 object-cover rounded"
-                    />
+                        <h2 className="text-black font-serif font-bold text-xl mb-2 mt-4">Caring for Cashmere</h2>
+                        <p className="text-gray-600 mb-4">
+                            Cashmere is a luxurious material and requires specific care. Follow our guide to ensure longevity.
+                        </p>
 
-                    <h2 className="text-black font-serif font-bold text-xl mb-2 mt-4">Why Choose Cashmere?</h2>
-                    <p className="text-gray-600 mb-4">
-                        Cashmere is a time-honored material cherished for its unmatched softness and durability. Learn why it's the choice of fashion enthusiasts around the world.
-                    </p>
+                        <ul className="list-disc pl-6 mb-4">
+                            <li className="mb-2">Wash with cold water using a gentle cycle or by hand.</li>
+                            <li className="mb-2">Avoid using bleach or fabric softeners on cashmere.</li>
+                            <li className="mb-2">Dry flat and keep away from direct sunlight or heat sources.</li>
+                            <li>Store in a cool, dry place.</li>
+                        </ul>
+
+                        <img 
+                            src={cashmereImage}
+                            alt="Cashmere Care"
+                            className="mb-4 w-full h-72 object-cover rounded"
+                        />
+
+                        <h2 className="text-black font-serif font-bold text-xl mb-2 mt-4">Why Choose Cashmere?</h2>
+                        <p className="text-gray-600 mb-4">
+                            Cashmere is a time-honored material cherished for its unmatched softness and durability. Learn why it's the choice of fashion enthusiasts around the world.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
