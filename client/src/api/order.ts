d486e6b8ac8098020f4419e8ee976743554812b3
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-05 13:26:18 +0100

    save

---
+++ client/src/api/order.ts
@@ -0,0 +1,35 @@
+import { IOrder } from "../types";
+import { handleErrors } from "../utils/handleApiErrors";
+import { instance } from "./instance";
+
+export const getAllOrders = async () => {
+    const { error, data } = await handleErrors(instance.get<IOrder[]>("/orders"));
+    if (error) {
+        throw new Error(error.message);
+    }
+    return data?.data as IOrder[];
+    }
+
+export const getOrderById = async (id: string) => {
+    const { error, data } = await handleErrors(instance.get<IOrder>(`/orders/${id}`));
+    if (error) {
+        throw new Error(error.message);
+    }
+    return data?.data as IOrder;
+}
+
+export const createOrder = async (order: IOrder) => {
+    const { error, data } = await handleErrors(instance.post<IOrder>("/orders", order));
+    if (error) {
+        throw new Error(error.message);
+    }
+    return data?.data as IOrder;
+}
+
+export const updateOrder = async (order: IOrder) => {
+    const { error, data } = await handleErrors(instance.put<IOrder>(`/orders/${order._id}`, order));
+    if (error) {
+        throw new Error(error.message);
+    }
+    return data?.data as IOrder;
+}
