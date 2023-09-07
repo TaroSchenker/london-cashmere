5b8881cdddd078fc19ae0c6ae79d7a6ad43b0a63
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-07 22:27:36 +0100

    fixed linting errors

--- client/src/features/Cart/components/tests/CartPage.test.tsx
+++ client/src/features/Cart/components/tests/CartPage.test.tsx
@@ -14,17 +14,53 @@ describe("CartPage", () => {
     onClose: mockOnClose,
   };
 
+  const closedProps = {
+    isOpen: false,
+    onClose: mockOnClose,
+  };
+
   // Mock the useAuth hook before your tests
   jest.spyOn(AuthHooks, "useAuth").mockReturnValue(mockUser);
 
-  it("should render the cart sidebar when isOpen is true", () => {
+  test("should render the cart sidebar when isOpen is true", () => {
     render(
       <BrowserRouter>
         <CartPage {...defaultProps} />
       </BrowserRouter>,
     );
-    const sidebar = screen.getByRole("complementary");
-    expect(sidebar).toBeInTheDocument();
+    const cartSideBar = screen.getByRole("complementary");
+    expect(cartSideBar).toBeInTheDocument();
+    expect(cartSideBar).toHaveClass("translate-x-0"); // Ensure the sidebar is visible
+  });
+
+  test("should not render the cart sidebar when isOpen is false", () => {
+    render(
+      <BrowserRouter>
+        <CartPage {...closedProps} />
+      </BrowserRouter>,
+    );
+    const cartSideBar = screen.getByRole("complementary");
+    expect(cartSideBar).toHaveClass("translate-x-full"); // Ensure the sidebar is hidden
+  });
+
+  test("should render the close button", () => {
+    render(
+      <BrowserRouter>
+        <CartPage {...defaultProps} />
+      </BrowserRouter>,
+    );
+    const closeButton = screen.getByRole("button");
+    expect(closeButton).toBeInTheDocument();
+  });
+
+  test("should render the cart products", () => {
+    render(
+      <BrowserRouter>
+        <CartPage {...defaultProps} />
+      </BrowserRouter>,
+    );
+    const cartProducts = screen.getAllByRole("listitem");
+    expect(cartProducts.length).toBeGreaterThan(0); // Ensure there are cart products
   });
 
   // ... rest of your tests
