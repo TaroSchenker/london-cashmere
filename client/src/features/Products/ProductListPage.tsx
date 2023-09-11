3bbcc6d189832cbb4d000402d9a0138fee97a5d1
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-11 11:07:31 +0100

    useProductLoader

--- client/src/features/Products/ProductListPage.tsx
+++ client/src/features/Products/ProductListPage.tsx
@@ -5,9 +5,11 @@ import SaleBanner from "../../common/layout/SaleBanner";
 import Footer from "./components/Footer/Footer";
 import ProductGrid from "./components/ProductGrid/ProductGrid";
 import { useLoaderData } from "react-router-dom";
+import { IProduct } from "../../types";
 
 const ProductListPage: React.FC = () => {
-  const { products } = useLoaderData();
+  const data = useLoaderData() as { products: IProduct[] };
+  const { products } = data;
   return (
     <div className="bg-background-color min-h-screen py-10">
       <div className="container mx-auto px-6">
--- client/src/hooks/useProductsLoader.ts
+++ client/src/hooks/useProductsLoader.ts
@@ -1,7 +1,11 @@
 import { getAllProducts } from "../api/products";
 
 export const useProductsLoader = async () => {
-  const products = await getAllProducts();
-  console.log("Loader test", products);
-  return { products };
+  try {
+    const products = await getAllProducts();
+    console.log("Loader test", products);
+    return { products };
+  } catch (e) {
+    console.log("use product loader error:", e);
+  }
 };
