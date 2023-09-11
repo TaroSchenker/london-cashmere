bc99871f0377901b1cd559a80f8d899772c24896
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-11 20:53:46 +0100

    fixed linting to push

--- client/src/common/layout/Header.tsx
+++ client/src/common/layout/Header.tsx
@@ -3,6 +3,7 @@ import Logo from "../components/Logo/Logo";
 import DesktopNav from "./DesktopNav";
 import DesktopIcons from "../components/Icons/DesktopIcons/DesktopIcons";
 import MobileIcons from "../components/Icons/MobileIcons/MobileIcons";
+import UserStatus from "../components/UserStatus/UserStatus";
 
 interface HeaderProps {
   sidebarOpen: boolean;
@@ -17,15 +18,29 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
   return (
     <div className="bg-background text-primary md:py-4">
-      <div className="container mx-auto px-4 md:flex md:justify-around md:items-center">
-        <Logo />
-        <DesktopNav />
-        <DesktopIcons setCartSidebarOpen={setCartSidebarOpen} />
-        <MobileIcons
-          sidebarOpen={sidebarOpen}
-          setSidebarOpen={setSidebarOpen}
-          setCartSidebarOpen={setCartSidebarOpen}
-        />
+      <div className="container mx-auto px-4">
+        {/* Upper Row for UserStatus */}
+        <div className="flex justify-end mb-2">
+          <UserStatus />
+        </div>
+
+        {/* Bottom Row for Logo, Navigation, and Icons */}
+        <div className="flex justify-between items-center">
+          <Logo />
+
+          {/* Centered DesktopNav */}
+          <DesktopNav />
+
+          {/* Icons Group */}
+          <div className="flex space-x-4 items-center">
+            <DesktopIcons setCartSidebarOpen={setCartSidebarOpen} />
+            <MobileIcons
+              sidebarOpen={sidebarOpen}
+              setSidebarOpen={setSidebarOpen}
+              setCartSidebarOpen={setCartSidebarOpen}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
