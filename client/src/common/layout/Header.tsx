9886f7a410e88b9ea3bf2125717d6d849c9c8ad6
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-11 20:55:36 +0100

    fixed lint

--- client/src/common/layout/Header.tsx
+++ client/src/common/layout/Header.tsx
@@ -3,6 +3,7 @@ import Logo from "../components/Logo/Logo";
 import DesktopNav from "./DesktopNav";
 import DesktopIcons from "../components/Icons/DesktopIcons/DesktopIcons";
 import MobileIcons from "../components/Icons/MobileIcons/MobileIcons";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import UserStatus from "../components/UserStatus/UserStatus";
 
 interface HeaderProps {
@@ -20,9 +21,7 @@ const Header: React.FC<HeaderProps> = ({
     <div className="bg-background text-primary md:py-4">
       <div className="container mx-auto px-4">
         {/* Upper Row for UserStatus */}
-        <div className="flex justify-end mb-2">
-          <UserStatus />
-        </div>
+        <div className="flex justify-end mb-2">{/* <UserStatus /> */}</div>
 
         {/* Bottom Row for Logo, Navigation, and Icons */}
         <div className="flex justify-between items-center">
