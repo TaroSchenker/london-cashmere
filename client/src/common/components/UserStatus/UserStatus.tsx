39572c79262a40f2b6f30b439b5fc31b608f7679
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-12 10:58:46 +0100

    fixed welcome and logout

--- client/src/common/components/UserStatus/UserStatus.tsx
+++ client/src/common/components/UserStatus/UserStatus.tsx
@@ -1,17 +1,36 @@
-// UserStatus.tsx
-
 import React from "react";
 import { useAuth } from "../../../hooks/useAuth"; // Adjust the path if necessary
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, Link } from "react-router-dom";
 
-const UserStatus: React.FC = () => {
-  const { currentUser } = useAuth();
+interface UserStatusProps {
+  setCartSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const UserStatus: React.FC<UserStatusProps> = ({ setCartSidebarOpen }) => {
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+    setCartSidebarOpen(false); // Assuming you want to close the cart sidebar on logout
+  };
 
   return (
-    <div className="user-status">
+    <div className="">
       {currentUser ? (
-        <span>Welcome, {currentUser.name}</span> // Assuming user object has a name property
+        <>
+          <span>Welcome, {currentUser.name}</span>
+          <button className="ml-4" onClick={handleLogout}>
+            <FontAwesomeIcon icon={faSignOutAlt} /> Logout
+          </button>
+        </>
       ) : (
-        <span>Not logged in</span>
+        <Link to="/user/auth">
+          <span>Login</span>
+        </Link>
       )}
     </div>
   );
