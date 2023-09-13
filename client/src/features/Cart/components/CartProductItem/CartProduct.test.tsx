ef40c65b4717f8d683d3b1bcd9673e116243d0fd
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-13 20:25:20 +0100

    updated cart product tests

--- client/src/features/Cart/components/CartProductItem/CartProduct.test.tsx
+++ client/src/features/Cart/components/CartProductItem/CartProduct.test.tsx
@@ -2,6 +2,8 @@ import CartProductItem from "./CartProductItem";
 
 import "@testing-library/jest-dom";
 import { customRender } from "../../../../__test__/helpers/renderWithProvider";
+import { screen } from "@testing-library/react";
+import { ICartItem } from "../../../../context/CartContext";
 
 const mockProduct = {
   _id: "product1",
@@ -29,7 +31,74 @@ describe("CartProductItem", () => {
     });
     const productImage = document.querySelector("img");
     console.log("productImage", productImage);
-    // expect(productImage).toHaveAttribute("src", mockProduct.imageUrl);
-    expect(1).toBe(1);
+    expect(productImage).toHaveAttribute("src", mockProduct.imageUrl);
+  });
+
+  test("renders product price correctly", () => {
+    customRender(<CartProductItem product={mockCartItem} />, {
+      withAuth: false,
+      withRouter: true,
+      withCart: true,
+    });
+
+    const productPrice = screen.getByText(
+      new RegExp(`Price: £${mockProduct.price}`),
+    );
+    expect(productPrice).toBeInTheDocument();
+  });
+
+  test("renders product quantity correctly", () => {
+    customRender(<CartProductItem product={mockCartItem} />, {
+      withAuth: false,
+      withRouter: true,
+      withCart: true,
+    });
+
+    const productQuantity = screen.getByText(
+      new RegExp(`Quantity: ${mockCartItem.quantity}`),
+    );
+    expect(productQuantity).toBeInTheDocument();
+  });
+
+  test("renders product name correctly", () => {
+    customRender(<CartProductItem product={mockCartItem} />);
+    const productName = screen.getByText(mockProduct.name);
+    expect(productName).toBeInTheDocument();
+  });
+
+  test("renders product description correctly", () => {
+    customRender(<CartProductItem product={mockCartItem} />);
+    const productDescription = screen.getByText(mockProduct.description);
+    expect(productDescription).toBeInTheDocument();
+  });
+
+  test("has correct alt attribute for product image", () => {
+    customRender(<CartProductItem product={mockCartItem} />);
+    const productImage = screen.getByAltText(mockProduct.name);
+    expect(productImage).toBeInTheDocument();
+  });
+
+  test("calculates and renders total price correctly", () => {
+    customRender(<CartProductItem product={mockCartItem} />);
+    const totalPrice = mockProduct.price * mockCartItem.quantity;
+    const displayedTotalPrice = screen.getByText(
+      new RegExp(`Total: £${totalPrice}`),
+    );
+    expect(displayedTotalPrice).toBeInTheDocument();
+  });
+
+  test("renders without crashing even if some properties are missing", () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const incompleteProduct: Partial<typeof mockProduct> = {
+      ...mockProduct,
+      description: undefined,
+    };
+    const incompleteCartItem = {
+      product: incompleteProduct,
+      quantity: 2,
+    };
+    customRender(<CartProductItem product={incompleteCartItem as ICartItem} />);
+    const productName = screen.getByText(mockProduct.name);
+    expect(productName).toBeInTheDocument();
   });
 });
