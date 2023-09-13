da70ff0da279e9d9177db06ea2c166ab346dca02
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-13 20:32:56 +0100

    cart total cost tests updated

---
+++ client/src/features/Cart/components/CartTotal/CartTotal.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { screen } from "@testing-library/react";
+import CartTotal from "./CartTotal"; // Adjust the path as needed
+import "@testing-library/jest-dom";
+import { customRender } from "../../../../__test__/helpers/renderWithProvider";
+
+describe("CartTotal", () => {
+  const mockTotalAmount = 150;
+
+  beforeEach(() => {
+    customRender(<CartTotal totalAmount={mockTotalAmount} />, {
+      withAuth: false,
+      withRouter: true,
+      withCart: false,
+    });
+  });
+
+  test("renders total amount correctly", () => {
+    const totalAmountText = screen.getByText(`Total: £${mockTotalAmount}`);
+    expect(totalAmountText).toBeInTheDocument();
+  });
+
+  test("Checkout link redirects to /checkout", () => {
+    const checkoutLink = screen.getByText("Checkout");
+    expect(checkoutLink).toHaveAttribute("href", "/checkout");
+  });
+});
