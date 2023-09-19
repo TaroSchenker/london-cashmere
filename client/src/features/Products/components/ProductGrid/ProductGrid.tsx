e81351d8996d4adbca21e1d57b0876cadc7c7f8a
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-19 14:12:21 +0100

    adjusted Product Link wraps. Only on img and title

--- client/src/features/Products/components/ProductGrid/ProductGrid.tsx
+++ client/src/features/Products/components/ProductGrid/ProductGrid.tsx
@@ -12,7 +12,7 @@ const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
       {/* Products grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
         {products.map((product, index) => (
-          <ProductCard key={index} product={product} />
+          <ProductCard key={product._id + index} product={product} />
         ))}
       </div>
     </div>
