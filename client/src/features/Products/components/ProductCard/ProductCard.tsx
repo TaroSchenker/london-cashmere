6340eb90513a0c86387fbb3954e463869a7dbcc5
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-21 10:16:42 +0100

    add to cart button implemented

--- client/src/features/Products/components/ProductCard/ProductCard.tsx
+++ client/src/features/Products/components/ProductCard/ProductCard.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import { IProduct } from "../../../../types/index";
-import { useAddToCart } from "../../../../hooks/useAddToCart";
+
 import { Link } from "react-router-dom";
 
-import { toast } from "react-toastify";
+import AddToCartButton from "../AddToCartButton/AddToCartButton";
 
 interface ProductCardProps {
   product: IProduct;
 }
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-  const addToCartHandler = useAddToCart(product);
-  const handleAddToCart = () => {
-    addToCartHandler();
-    toast.success(`${product.name} added to cart`, {
-      position: toast.POSITION.TOP_LEFT,
-      autoClose: 1000,
-    });
-  };
-
   return (
     <div className="border border-gray-300 rounded-lg p-6 hover:shadow-xl transition-shadow duration-300 bg-white">
       <div className="relative group">
@@ -53,27 +44,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       </div>
 
       <div className="flex mt-4 items-center">
-        {/* <div className="flex-1">
-          <span className="text-sm text-gray-500">Colors: </span>
-          {product.color.map((c, index) => (
-            <span
-              key={index}
-              className="inline-block w-5 h-5 mr-2 rounded-full"
-              style={{ backgroundColor: c }}
-            ></span>
-          ))}
-        </div> */}
-        <button
-          className={`bg-black text-white px-4 py-2 rounded transition-transform duration-300 transform ${
-            product.stockCount === 0
-              ? "opacity-50 cursor-not-allowed"
-              : "hover:scale-105"
-          }`}
-          disabled={product.stockCount === 0}
-          onClick={handleAddToCart}
-        >
-          Add to Cart
-        </button>
+        <AddToCartButton product={product} />
       </div>
     </div>
   );
--- client/src/features/Products/components/ProductInfo/ProductInfo.tsx
+++ client/src/features/Products/components/ProductInfo/ProductInfo.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import { IProduct } from "../../../../types";
-import { useAddToCart } from "../../../../hooks/useAddToCart";
-import { toast } from "react-toastify";
+import AddToCartButton from "../AddToCartButton/AddToCartButton";
 
 interface ProductInfoProps {
   product: IProduct;
 }
 
 const ProductInfo: React.FC<ProductInfoProps> = ({ product }) => {
-  const addToCartHandler = useAddToCart(product);
-  const handleAddToCart = () => {
-    addToCartHandler();
-    toast.success(`${product.name} added to cart`, {
-      position: toast.POSITION.TOP_LEFT,
-      autoClose: 1000,
-    });
-  };
-
   return (
     <div className="p-6 w-full md:w-1/2">
       <h2 className="text-black font-serif font-bold text-2xl mb-2">
@@ -31,32 +21,11 @@ const ProductInfo: React.FC<ProductInfoProps> = ({ product }) => {
         </span>
       </div>
 
-      {/* <div className="mb-4 flex items-center flex-wrap">
-        <span className="text-sm text-gray-500 mr-2">Colors:</span>
-        {product.color.map((c, index) => (
-          <span
-            key={index}
-            className="inline-block w-6 h-6 mr-1 mt-1 rounded-full"
-            style={{ backgroundColor: c }}
-          ></span>
-        ))}
-      </div> */}
-
       <div className="flex items-center justify-between mb-4">
         <span className="font-serif font-bold text-2xl text-black">
           £{product.price.toFixed(2)}
         </span>
-        <button
-          className={`bg-black text-white px-6 py-2 rounded transition-transform duration-300 transform ${
-            product.stockCount === 0
-              ? "opacity-50 cursor-not-allowed"
-              : "hover:scale-105"
-          }`}
-          disabled={product.stockCount === 0}
-          onClick={handleAddToCart}
-        >
-          Add to Cart
-        </button>
+        <AddToCartButton product={product} />
       </div>
 
       <div className="border-t pt-4 mb-4">
