8109c7d68ae96100fe224ac924698b53d62d9326
Taro Schenker <taroschenker@hotmail.co.uk>
2023-09-27 13:23:58 +0100

    changed axios instance for prod

--- client/src/api/instance.ts
+++ client/src/api/instance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3001/api";
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://london-cashmere-server.onrender.com/api";
 
 export const instance = axios.create({
   baseURL: API_BASE_URL,
