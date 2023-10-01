8795b3ca2ad756cedbef817862cd81d8ad1949a3
Taro Schenker <taroschenker@hotmail.co.uk>
2023-10-01 11:35:11 +0100

    improved go back button

--- client/src/features/Products/ProductDetailPage.tsx
+++ client/src/features/Products/ProductDetailPage.tsx
@@ -8,23 +8,30 @@ const ProductDetailPage: React.FC = () => {
   const [isZoomed, setIsZoomed] = useState(false);
   const navigate = useNavigate();
   const data = useLoaderData() as { product: IProduct };
+
   if (!data || !data.product) {
     return <div>Error loading product</div>;
   }
+
   const { product } = data;
 
   return (
-    <div className="p-4 md:p-8 bg-background-color flex flex-col md:flex-row justify-center items-center">
-      {/* go back button */}
-      <div className="mb-4 md:mb-0 md:absolute md:top-1/2 md:-translate-y-1/2 md:left-8 bg-primary-color text-background-color px-4 py-2 rounded transform">
-        <button onClick={() => navigate(-1)}>← Go Back</button>
-      </div>
+    <div className="relative pb-10 md:p-8 bg-background-color flex flex-col md:flex-row justify-center items-start">
+      {/* Go Back Button */}
+      <button
+        onClick={() => navigate(-1)}
+        className="m-4 bg-primary-color text-background-color px-4 py-2 rounded relative top-4 left-4 md:relative md:top-auto md:left-auto md:mb-4"
+      >
+        ← Go Back
+      </button>
+
       <div className="max-w-full md:max-w-4xl w-full border border-gray-300 rounded-lg hover:shadow-xl transition-shadow duration-300 bg-white flex flex-col md:flex-row">
         <ImageDisplay
           product={product}
           isZoomed={isZoomed}
           toggleZoom={() => setIsZoomed(!isZoomed)}
         />
+
         <ProductInfo product={product} />
       </div>
     </div>
