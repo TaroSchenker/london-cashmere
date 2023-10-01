521f1ba5519ca17e0241a1f8505606bad8a64512
Taro Schenker <taroschenker@hotmail.co.uk>
2023-10-01 12:38:30 +0100

    commented out dummy data

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -16,7 +16,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   hashedPassword: { type: String, required: true },
-  address: { type: String, required: true },
+  address: { type: String, required: false },
   orderHistory: [{ type: Schema.Types.ObjectId, ref: "Order" }],
   role: {
     type: String,
